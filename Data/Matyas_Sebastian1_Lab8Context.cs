e463af75d5ad623376bcb7b2207925a4ea87f218
SEB <SEB@VERSACE>
2020-12-02 15:22:18 +0200

    Laboratorul 9

--- Data/Matyas_Sebastian1_Lab8Context.cs
+++ Data/Matyas_Sebastian1_Lab8Context.cs
@@ -1,15 +1,10 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
-using Microsoft.EntityFrameworkCore;
-using Matyas_Sebastian1_Lab8.Models;
+﻿using Microsoft.EntityFrameworkCore;
 
 namespace Matyas_Sebastian1_Lab8.Data
 {
     public class Matyas_Sebastian1_Lab8Context : DbContext
     {
-        public Matyas_Sebastian1_Lab8Context (DbContextOptions<Matyas_Sebastian1_Lab8Context> options)
+        public Matyas_Sebastian1_Lab8Context(DbContextOptions<Matyas_Sebastian1_Lab8Context> options)
             : base(options)
         {
         }
@@ -19,5 +14,7 @@ namespace Matyas_Sebastian1_Lab8.Data
         public DbSet<Matyas_Sebastian1_Lab8.Models.Publisher> Publisher { get; set; }
 
         public DbSet<Matyas_Sebastian1_Lab8.Models.Category> Category { get; set; }
+
+        public DbSet<Matyas_Sebastian1_Lab8.Models.BookCategory> BookCategory { get; set; }
     }
 }
